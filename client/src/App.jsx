b5b56706759a8688ba6b3e602d95ed5fcda8d4ef
KILLERNEGAN <kisarasirimanne21@gmail.com>
2026-03-03 20:20:38 +0530

    Remove unnecessary whitespace and add comment for clarity in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
-
+//wethmin
 function App() {
   return (
     <BrowserRouter>
